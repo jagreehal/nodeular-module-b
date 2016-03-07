ca6260d7b221bc6540857f68ecb48e0ea981ccc1
Jag Reehal <jag.reehal@gmail.com>
2016-03-07 20:11:22 +0000

    Now passes in app

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var app = require('nodeular-core/express');
 var api = require('./api');
 
-// anti pattern
-app.get('/b', function (req, res){
-  setTimeout(function (){
-    var date = res.locals.dateStamp || new Date();
-    return res.status(200).send('b - ' + date);
-  }, 1000);
-});
 
-app.use('/b/api', api);
+function init(app) {
 
-module.exports = app;
+  app.get('/b', function (req, res) {
+    setTimeout(function () {
+      var date = res.locals.dateStamp || new Date();
+      return res.status(200).send('b - ' + date);
+    }, 1000);
+  });
+
+  app.use('/b/api', api);
+}
+
+module.exports = init;
